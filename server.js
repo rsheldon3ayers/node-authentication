b34ea5a654c8da23f14a406a1e3a4c9d4c80d9c4
Robert Ayers <rsheldon3ayers@gmail.com>
2016-02-22 10:00:29 -0600

    made register, login, server, etc

---
+++ server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+
+app.post('/login', (req, res) => {
+  res.redirect('/');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  if(req.body.password === req.body.verify) {
+  res.redirect('login');
+} else {
+  res.render('register', {
+    email: req.body.email,
+    message: 'Passwords do not match'
+    });
+  };
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
