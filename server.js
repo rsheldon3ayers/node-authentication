cda4a42ea3476e61e5691073bcc07bbcd6066fb4
Robert Ayers <rsheldon3ayers@gmail.com>
2016-02-23 09:59:29 -0600

    made logout button

--- server.js
+++ server.js
@@ -2,49 +2,42 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 
+const userRoutes = require('./lib/user/routes');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const SESSION_SECRET = process.env.SESSION_SECRET || 'supersecret';
+const SESSION_SECRET = process.env.SESSION_SECRET || 'secret';
 
 app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 app.use(session({
   secret: SESSION_SECRET,
   store: new RedisStore()
 }));
+app.use(userRoutes);
+
+app.locals.title = '';
+
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || { email: 'Guest' };
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/login', (req, res) => {
-  res.render('login');
-});
+mongoose.connect('mongodb://localhost:27017/nodeauth', (err) => {
+  if (err) throw err;
 
-
-app.post('/login', (req, res) => {
-  res.redirect('/');
-});
-
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
-app.post('/register', (req, res) => {
-  if(req.body.password === req.body.verify) {
-  res.redirect('login');
-} else {
-  res.render('register', {
-    email: req.body.email,
-    message: 'Passwords do not match'
-    });
-  };
-});
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
 });
